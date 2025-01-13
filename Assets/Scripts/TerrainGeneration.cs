c037b86ae52fb0b4e8f951f2f969453968bf7117
LamoteKobe <kobe.lamote@student.howest.be>
2025-01-13 14:01:57 +0100

    Terrain generation V0.1

---
+++ Assets/Scripts/TerrainGeneration.cs
@@ -0,0 +1,108 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGeneration : MonoBehaviour
+{
+    private Terrain terrain;
+    private TerrainData terrainData;
+    private int height;
+    private int width;
+
+    // Bezier
+    private Vector3[] controlPoints = new Vector3[11];
+    private List<Vector3> bezierPoints = new List<Vector3>();
+
+
+    public int worldHeight = 10;
+
+    void Start()
+    {
+        terrain = GetComponent<Terrain>();
+        terrainData = terrain.terrainData;
+        width = terrainData.heightmapResolution;
+        height = terrainData.heightmapResolution;
+        worldHeight += 10;
+
+
+        GenerateRandomBezierCurve();
+        CarveTerrain();
+    }
+    void CarveTerrain()
+    {
+        float[,] heights = new float[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                heights[x, y] = worldHeight / terrainData.size.y;
+                foreach (var point in bezierPoints)
+                {
+                    float Newx = point.x + 512;
+                    float Newz = point.z + 512;
+                    if (Vector2.Distance(new Vector2(Newz, Newx), new Vector2(x, y)) < 5)
+                    {
+                        heights[x, y] = -15f;
+                    }
+                }
+            }
+        }
+        terrainData.SetHeights(0, 0, heights);
+    }
+
+    void GenerateRandomBezierCurve()
+    {
+        controlPoints[0] = new Vector3(0, 0, 0);
+        controlPoints[1] = new Vector3(0, 0, 50);
+        controlPoints[2] = new Vector3(0, 0, 100);
+
+        for (int i = 2; i < controlPoints.Length; i++)
+        {
+            float randomX = Random.Range(-200f, 200f);
+            float z = i * 50;
+            controlPoints[i] = new Vector3(randomX, 0, z);
+        }
+
+        for (int i = 0; i < controlPoints.Length - 3; i += 3)
+        {
+            Vector3 previousPoint = controlPoints[i];
+
+            for (float t = 0; t <= 1; t += 0.01f)
+            {
+                Vector3 point = CalculateBezierPoint(t, controlPoints[i], controlPoints[i + 1], controlPoints[i + 2], controlPoints[i + 3]);
+                bezierPoints.Add(point);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        for (int i = 0; i < controlPoints.Length - 3; i += 3)
+        {
+            Vector3 previousPoint = controlPoints[i];
+
+            for (float t = 0; t <= 1; t += 0.01f)
+            {
+                Vector3 point = CalculateBezierPoint(t, controlPoints[i], controlPoints[i + 1], controlPoints[i + 2], controlPoints[i + 3]);
+                Gizmos.DrawLine(previousPoint, point);
+                previousPoint = point;
+            }
+        }
+    }
+
+    Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
+
+        Vector3 p = uuu * p0; // (1-t)^3 * p0
+        p += 3 * uu * t * p1; // 3 * (1-t)^2 * t * p1
+        p += 3 * u * tt * p2; // 3 * (1-t) * t^2 * p2
+        p += ttt * p3; // t^3 * p3
+
+        return p;
+    }
+}
