d0cf761c3e3bff72682229ca052bfac99f238946
KleeWout <145778685+KleeWout@users.noreply.github.com>
2025-01-13 14:13:22 +0100

    /feat: add lives & collisions

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        //when hitting the logs
+        if (collision.collider.tag == "Obstacle")
+        {
+            HealthManager.health--;
+            if (HealthManager.health <= 0)
+            {
+                Debug.Log("Game Over!");
+            }
+            else
+            {
+                StartCoroutine(GetHurt());
+            }
+
+
+        }
+        Debug.Log(collision.collider.tag);
+
+    }
+    IEnumerator GetHurt()
+    {
+
+        Physics.IgnoreLayerCollision(9, 10, true);
+        GetComponent<Animator>().SetLayerWeight(1, 1);
+        Debug.Log("Ignoring collision");
+        yield return new WaitForSeconds(2);
+        GetComponent<Animator>().SetLayerWeight(1, 0);
+        Debug.Log("Turning off animation");
+        Physics.IgnoreLayerCollision(9, 10, false);
+    }
+}
