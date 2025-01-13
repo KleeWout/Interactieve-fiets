730a09863a9c61c410071e9f20b43cf5f9f89161
KleeWout <145778685+KleeWout@users.noreply.github.com>
2025-01-13 16:16:48 +0100

    /fix: levels

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -27,11 +27,11 @@ public class PlayerCollision : MonoBehaviour
     {
 
         Physics.IgnoreLayerCollision(9, 10, true);
-        GetComponent<Animator>().SetLayerWeight(1, 1);
+        // GetComponent<Animator>().SetLayerWeight(1, 1);
         Debug.Log("Ignoring collision");
         yield return new WaitForSeconds(2);
-        GetComponent<Animator>().SetLayerWeight(1, 0);
-        Debug.Log("Turning off animation");
+        // GetComponent<Animator>().SetLayerWeight(1, 0);
+        // Debug.Log("Turning off animation");
         Physics.IgnoreLayerCollision(9, 10, false);
     }
 }
