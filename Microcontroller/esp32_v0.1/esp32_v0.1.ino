ccbd23e07627dbe2c41b46c90dcb9677a13e634a
LamoteKobe <kobe.lamote@student.howest.be>
2025-01-15 13:25:58 +0100

    update esp script for backwards

--- Microcontroller/esp32_v0.1/esp32_v0.1.ino
+++ Microcontroller/esp32_v0.1/esp32_v0.1.ino
@@ -5,76 +5,98 @@
 #define REED_PIN_R_S 25
 
 
-const unsigned long interval = 200;
-const unsigned long debounceDelay = 600;  
-
 // left
-unsigned long lastDebounceTimeLeft = 0;
-unsigned long lastRevolutionLeft = 0;
-unsigned long previousRevolutionLeft = 0;
+bool leftMainStatus = false;
+bool leftSecondStatus = false;
+bool leftReverse = false;
+
+unsigned long lastRevolutionLeftMain = 0;
+unsigned long lastRevolutionLeftSecond = 0;
+
+// maybe remove later
+unsigned long previousRevolutionLeftMain = 0;
+
 float revolutionsPerSecondLeft = 0;
 
-// right
-unsigned long lastDebounceTimeRight = 0;
-unsigned long lastRevolutionRight = 0;
-unsigned long previousRevolutionRight = 0;
-float revolutionsPerSecondRight = 0;
-
-
-void IRAM_ATTR handleRevolutionLeft() {
+// deze moet eerst
+void IRAM_ATTR handleRevolutionLeftMain() {
 
   unsigned long currentTime = millis();
 
-  if (( currentTime - lastDebounceTimeLeft) > debounceDelay) {
-    previousRevolutionLeft = lastRevolutionLeft;
-    lastDebounceTimeLeft = millis();
-    revolutionsPerSecondLeft = 1000.0 / (currentTime - lastRevolutionLeft);
-    lastRevolutionLeft = millis();
+  if(leftSecondStatus == false){
+
+    // maybe remove later
+    previousRevolutionLeftMain = lastRevolutionLeftMain;
+
+
+    leftSecondStatus = true;
+    leftMainStatus = false;
+    lastRevolutionLeftMain = currentTime;
+
+    if(leftReverse){
+      if(lastRevolutionLeftMain-lastRevolutionLeftSecond>100){
+        leftReverse = false;
+      }
+      else{
+        revolutionsPerSecondLeft = (-85.0/(lastRevolutionLeftMain-lastRevolutionLeftSecond))/3;
+
+
+        // Serial.println("snelheid = -" + String(lastRevolutionLeftMain-lastRevolutionLeftSecond));
+      }
+    }
   }
+
 }
-void IRAM_ATTR handleRevolutionRight() {
+
+// dan kort erna deze
+void IRAM_ATTR handleRevolutionLeftSecond() {
 
   unsigned long currentTime = millis();
 
-  if (( currentTime - lastDebounceTimeRight) > debounceDelay) {
-    previousRevolutionRight = lastRevolutionRight;
-    lastDebounceTimeRight = millis();
-    revolutionsPerSecondRight = 1000.0 / (currentTime - lastRevolutionRight);
-    lastRevolutionRight = millis();
+  if(leftMainStatus == false){
+    leftMainStatus = true;
+    leftSecondStatus = false;
+    lastRevolutionLeftSecond = currentTime;
+    if(lastRevolutionLeftSecond-lastRevolutionLeftMain>500){
+      leftReverse = true;
+    }else{
+      leftReverse = false;
+      revolutionsPerSecondLeft = 85.0/(lastRevolutionLeftSecond-lastRevolutionLeftMain);
+
+
+
+      // Serial.println("snelheid = " + String(85.0/(lastRevolutionLeftSecond-lastRevolutionLeftMain)));
+      // Serial.println("toeren per seconde = " + String(1000.0 / (currentTime - previousRevolutionLeftMain)));
+    }
   }
+
 }
-
-
 
 void setup() {
 
   Serial.begin(115200);
+
   pinMode(REED_PIN_L_M, INPUT_PULLUP);
   pinMode(REED_PIN_L_S, INPUT_PULLUP);
   pinMode(REED_PIN_R_M, INPUT_PULLUP);
   pinMode(REED_PIN_R_S, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(REED_PIN_L_M), handleRevolutionLeft, RISING);
-  attachInterrupt(digitalPinToInterrupt(REED_PIN_R_M), handleRevolutionRight, RISING);
 
-  delay(500);
-
-  Serial.println("v0.1");
+  attachInterrupt(digitalPinToInterrupt(REED_PIN_L_M), handleRevolutionLeftMain, RISING);
+  attachInterrupt(digitalPinToInterrupt(REED_PIN_L_S), handleRevolutionLeftSecond, RISING);
 
 }
 
 void loop() {
-
-  if(millis() > lastRevolutionLeft + (lastRevolutionLeft - previousRevolutionLeft) && lastRevolutionLeft != 0){
-    revolutionsPerSecondLeft = 1000.0 / (millis() - lastRevolutionLeft);
+  if(!leftReverse && millis() > lastRevolutionLeftMain + (lastRevolutionLeftMain - previousRevolutionLeftMain) && lastRevolutionLeftMain != 0){
+    revolutionsPerSecondLeft = (1000.0 / (millis() - lastRevolutionLeftMain))/2;
   }
-  if(millis() > lastRevolutionRight + (lastRevolutionRight - previousRevolutionRight) && lastRevolutionRight != 0){
-    revolutionsPerSecondRight = 1000.0 / (millis() - lastRevolutionRight);
+  if(leftReverse && millis() > lastRevolutionLeftMain + (lastRevolutionLeftMain - previousRevolutionLeftMain) && lastRevolutionLeftMain != 0){
+    revolutionsPerSecondLeft = (-(1000.0 / (millis() - lastRevolutionLeftMain))/2)/3;
   }
 
-  delay(interval);
+  delay(100);
+  Serial.println(revolutionsPerSecondLeft);
 
-  // Serial.println(revolutionsPerSecondLeft + String(",") + revolutionsPerSecondRight);
-  // Serial.println(String(revolutionsPerSecondLeft) +","+ String(revolutionsPerSecondRight));
 
 }
