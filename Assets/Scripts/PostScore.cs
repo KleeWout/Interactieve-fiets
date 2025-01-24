170e402f9c015e4848fcec7170b9eda0b8096c4d
KleeWout <145778685+KleeWout@users.noreply.github.com>
2025-01-24 10:30:48 +0100

    /feat script for posting score

---
+++ Assets/Scripts/PostScore.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class MyBehavior : MonoBehaviour
+{
+
+    private static string url = "https://entertainendefietsgameleaderboard.azurewebsites.net/api/leaderboard?code=Q8dqx9qcNI8yuXDZOCWP05B8pC7fZED6ymj4S5RHVFMPAzFuOZqv8w==";
+
+
+    IEnumerator Upload(string name, int score)
+    {
+        string body = "{ \"name\": \"" + name + "\", \"score\": " + score + " }";
+        using (UnityWebRequest www = UnityWebRequest.Post(url, body, "application/json"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+    }
+}
