1ac9a4982dc01045c67ed85b749306b4db8e37fc
LamoteKobe <kobe.lamote@student.howest.be>
2025-01-24 13:20:45 +0100

    Update SerialController.cs

--- Assets/Ardity/Scripts/SerialController.cs
+++ Assets/Ardity/Scripts/SerialController.cs
@@ -75,7 +75,8 @@ public class SerialController : MonoBehaviour
         thread = new Thread(new ThreadStart(serialThread.RunForever));
         thread.Start();
         string expectedMessage = "v0.2";
-        DateTime timeout = DateTime.Now.AddSeconds(2);
+        DateTime timeout = DateTime.Now.AddSeconds(10);
+        serialThread.SendMessage("getversion");
 
         while (DateTime.Now < timeout)
         {
